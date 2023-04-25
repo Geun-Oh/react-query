47bb594d10cb36a1ea9f686b110fef1fb62efe99
Geun-Oh <kandy1002@naver.com>
2023-04-25 14:29:16 +0900

    feat: 간단하게 CRUD 구현

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,39 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const d = require("./db.json");
+
+const data = d;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(data);
 });
 
 app.post("/", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+  const userprofile = data.userprofile;
+  const newData = {
+    id: userprofile.length + 1,
+    ...req.body,
+  };
+  userprofile.push(newData);
+  res.send(userprofile);
+});
+
+app.put("/", (req, res) => {
+  const { id, ...putData } = req.body;
+  data.userprofile[id - 1] = {
+    ...data.userprofile[id - 1],
+    ...putData,
+  };
+  res.send(data.userprofile);
+});
+
+app.delete("/", (req, res) => {
+  const { id } = req.body;
+  data.userprofile = data.userprofile.filter((x) => x.id !== id);
+  res.send(data.userprofile);
 });
 
 app.listen(port, () => {
