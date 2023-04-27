67a68b21ca915943a89a5cd059cce10e8a10414f
Geun-Oh <kandy1002@naver.com>
2023-04-27 11:39:22 +0900

    fix: 함수 관련 conflict 해결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,8 +7,9 @@ function App() {
   // get, post, put 쿼리를 가져와주세요!
   // get 쿼리에서 필요한 데이터를 구조분해 할당으로 가져와주세요!
 
+  // 추가 수정 로직을 분기해주는 함수입니다! 여기 있는 에러를 모두 지워주세요!
   const mutateFn: (props: userProfile) => void = (props: userProfile) => {
-    const match = data?.data.userprofile.filter(
+    const match = data.data.userprofile.filter(
       ({ name }: { name: string }) => name === props.name
     );
     return match.length === 1 ? PUT.mutate(props) : POST.mutate(props);
@@ -29,7 +30,7 @@ function App() {
         <section className="profile">
           <h2>프로필 목록</h2>
           <section>
-            {data?.data.userprofile.map(
+            {data.data.userprofile.map(
               ({ name, nickname, mbti, birth, instagram }: userProfile) => (
                 <ProfileCard
                   name={name}
--- frontend/src/api/hooks/profile.tsx
+++ frontend/src/api/hooks/profile.tsx
@@ -1,15 +1,15 @@
 export const useQueryProfile = () => {
-  const baseURL = 'http://localhost:3000';
+  const baseURL = "http://localhost:3000";
 
   // QueryClient를 가져와주세요!
 
-  const GET = ; // 프로필 조회
+  const GET = () => "get 함수를 작성해주세요!"; // 프로필 조회
 
-  const POST = ; // 프로필 추가
+  const POST = () => "post 함수를 작성해주세요!"; // 프로필 추가
 
-  const PUT = ; // 프로필 수정
+  const PUT = () => "put 함수를 작성해주세요!"; // 프로필 수정
 
-  const DELETE = ; // 프로필 삭제
+  const DELETE = () => "delete 함수를 작성해주세요!"; // 프로필 삭제
 
   return {
     GET,
